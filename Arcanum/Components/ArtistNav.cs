c613a69527b7b0bb816faaf84d017132dd795404
Scott Falbo <scottfalboart@gmail.com>
2021-09-14 17:16:47 -0700

    Built ArtistNav component to handle dynamic rendering of links in _Layout.cshtml

---
+++ Arcanum/Components/ArtistNav.cs
@@ -0,0 +1,46 @@
+﻿using Arcanum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arcanum.Components
+{
+    [ViewComponent]
+    public class ArtistNavViewComponent : ViewComponent
+    { 
+
+        public ArtistNavViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Artist> artists = new List<Artist>();
+            artists.Add(dummyData("spaceghost"));
+            artists.Add(dummyData("harry"));
+            artists.Add(dummyData("luci"));
+            artists.Add(dummyData("ethel"));
+
+            ViewModel viewModel = new ViewModel()
+            {
+                Artists = artists
+            };
+
+            return View(viewModel);
+        }
+
+        private Artist dummyData(string name) =>
+            new Artist() { Name = name };
+
+        public class ViewModel
+        {
+            public List<Artist> Artists { get; set; }
+        }
+    }
+
+
+
+}
