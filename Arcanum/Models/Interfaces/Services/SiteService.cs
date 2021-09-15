1d93d9a34c582008a9db5e0c27f89ac5363cd38b
Scott Falbo <scottfalboart@gmail.com>
2021-09-14 23:51:13 -0700

    Added Artist table cruds to ISite and built on the associated methods in SiteService.  create, read, update, delete complete.

--- Arcanum/Models/Interfaces/Services/SiteService.cs
+++ Arcanum/Models/Interfaces/Services/SiteService.cs
@@ -16,6 +16,10 @@ namespace Arcanum.Models.Interfaces.Services
             _db = context;
         }
 
+        /// <summary>
+        /// Create a new artist record in the database with the user registration data.
+        /// </summary>
+        /// <param name="artist"> Artist object </param>
         public async Task CreateArtist(Artist artist)
         {
             Artist newArtist = new Artist()
@@ -30,6 +34,37 @@ namespace Arcanum.Models.Interfaces.Services
             await _db.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Query an Artist record from the database by id.
+        /// </summary>
+        /// <param name="id"> string Artist.Id </param>
+        /// <returns> Artist object </returns>
+        public async Task<Artist> GetArtist(string id)
+        {
+            return await _db.Artist
+                .Where(x => x.Id == id)
+                .Include(a => a.ArtistPortfolios)
+                .ThenInclude(b => b.Portfolio)
+                .ThenInclude(c => c.PortfolioImage)
+                .ThenInclude(d => d.Image)
+                .Include(e => e.ArtistBooking)
+                .ThenInclude(f => f.Booking)
+                .Select(g => new Artist
+                {
+                    Id = g.Id,
+                    Name = g.Name,
+                    Order = g.Order,
+                    Email = g.Email,
+                    Display = g.Display,
+                    ArtistPortfolios = g.ArtistPortfolios,
+                    ArtistBooking = g.ArtistBooking
+                }).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Query a List<Artist> of all Artist records in the database.
+        /// </summary>
+        /// <returns> List<Artist> </returns>
         public async Task<List<Artist>> GetArtists()
         {
             return await _db.Artist
@@ -50,5 +85,29 @@ namespace Arcanum.Models.Interfaces.Services
                     ArtistBooking = g.ArtistBooking
                 }).ToListAsync();
         }
+
+        /// <summary>
+        /// Update an artist record.
+        /// </summary>
+        /// <param name="artist"> Artist object </param>
+        public async Task UpdateArtist(Artist artist)
+        {
+            _db.Entry(artist).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Delete an Artist record by id.
+        /// </summary>
+        /// <param name="id"> string Artist.Id </param>
+        public async Task DeleteArtist(string id)
+        {
+            Artist artist = await _db.Artist.FindAsync(id);
+            _db.Entry(artist).State = EntityState.Deleted;
+            await _db.SaveChangesAsync();
+
+            //TODO: Need to add logic to delete portfoios, images and booking page once they are built.
+        }
+
     }
 }
