28590e0e3a17df89240156b6113e3e8ae2062c09
Scott Falbo <scottfalboart@gmail.com>
2021-09-17 23:42:22 -0700

    Added a studio view in the pages.

--- Arcanum/Pages/Index.cshtml.cs
+++ Arcanum/Pages/Index.cshtml.cs
@@ -22,11 +22,12 @@ namespace Arcanum.Pages
         }
 
         [BindProperty]
-        public Models.ArcanumMain MainPage { get; set; }
+        public ArcanumMain MainPage { get; set; }
+        public string Instagram { get; set; }
 
         public async Task OnGet()
         {
-            MainPage = await _siteAdmin.GetMainPage();
+            await RefreshPage();
         }
 
         /// <summary>
@@ -37,8 +38,18 @@ namespace Arcanum.Pages
             if (MainPage.IntroA == null) MainPage.IntroA = " ";
             if (MainPage.IntroB == null) MainPage.IntroB = " ";
             await _siteAdmin.UpdateMainPage(MainPage);
-            MainPage = await _siteAdmin.GetMainPage();
+
+            await RefreshPage();
             Redirect("/");
+        }
+
+        /// <summary>
+        /// Helper method to refresh the page properties when refreshed without an IActionResult.
+        /// </summary>
+        private async Task RefreshPage()
+        {
+            Instagram = (await _siteAdmin.GetStudio()).Instagram;
+            MainPage = await _siteAdmin.GetMainPage();
         }
     }
 }
--- Arcanum/Pages/TheStudio.cshtml.cs
+++ Arcanum/Pages/TheStudio.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Arcanum.Models;
+using Arcanum.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace Arcanum.Pages
 {
     public class TheStudioModel : PageModel
     {
-        public void OnGet()
+        private readonly ISite _siteAdmin;
+
+        public TheStudioModel(ISite siteAdmin)
         {
+            _siteAdmin = siteAdmin;
+        }
+
+        public StudioInfo StudioInfo { get; set; }
+
+        public async Task OnGet()
+        {
+            StudioInfo = await _siteAdmin.GetStudio();
         }
     }
 }
