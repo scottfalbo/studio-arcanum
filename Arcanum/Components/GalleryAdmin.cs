640380c2734bb848bb7c6609d1d443ea6661b7fb
Scott Falbo <scottfalboart@gmail.com>
2021-12-10 15:49:20 -0800

    Thumbnails rendering in galery admin component.

--- Arcanum/Components/GalleryAdmin.cs
+++ Arcanum/Components/GalleryAdmin.cs
@@ -1,4 +1,5 @@
-﻿using Arcanum.Models;
+﻿using Arcanum.Data;
+using Arcanum.Models;
 using Arcanum.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,19 +12,28 @@ namespace Arcanum.Components
     [ViewComponent]
     public class GalleryAdminViewComponent : ViewComponent
     {
+        private readonly ArcanumDbContext _db;
         public IArtistAdmin _artistAdmin;
 
-        public GalleryAdminViewComponent(IArtistAdmin artistAdmin)
+        public GalleryAdminViewComponent(IArtistAdmin artistAdmin, ArcanumDbContext db)
         {
             _artistAdmin = artistAdmin;
+            _db = db;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(int portfolioId)
         {
+            List<PortfolioImage> portfolioImages = await _artistAdmin.GetPortfolioImages(portfolioId);
+            List<Image> images = new List<Image>();
+            foreach(PortfolioImage image in portfolioImages)
+            {
+                images.Add(await _db.Image.FindAsync(image.ImageId));
+            }
+
             ViewModel viewModel = new ViewModel
             {
                 PortfolioId = portfolioId,
-                Images = 
+                Images = images
             };
             return View(viewModel);
         }
@@ -31,7 +41,7 @@ namespace Arcanum.Components
         public class ViewModel
         {
             public int PortfolioId { get; set; }
-            public List<PortfolioImage> Images { get; set; }
+            public List<Image> Images { get; set; }
         }
     }
 }
