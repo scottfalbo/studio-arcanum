8351115bcd6a8a55427add7a1fda3de55608a7e7
Scott Falbo <scottfalboart@gmail.com>
2022-02-10 22:36:04 -0800

    input form password peek implemented

--- Arcanum/wwwroot/js/registrationValidator.js
+++ Arcanum/wwwroot/js/registrationValidator.js
@@ -6,15 +6,15 @@ let passwordIsMatch = false;
 // Validation to ensure that the user enters the same password
 // in both fields during account creation.
 $(function () {
-    $(".password-input").keyup(validatePassword);
+    $('.password-input').keyup(validatePassword);
 });
 $(function () {
-    $(".password-validator").keyup(validatePassword);
+    $('.password-validator').keyup(validatePassword);
 });
 
 function validatePassword() {
-    var password1 = $(".password-input").val();
-    var password2 = $(".password-validator").val();
+    let password1 = $('.password-input').val();
+    let password2 = $('.password-validator').val();
     if (password1 === password2) {
         $('.password-valid-status').text('O');
         passwordIsMatch = true;
@@ -43,7 +43,7 @@ function validateEmail() {
     }
     validateAllInputs();
 }
-
+// Regex to check format.
 function checkFormat(email) {
     let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
     return emailPattern.test(email);
@@ -60,9 +60,41 @@ function validateAllInputs() {
     }
 }
 
+// Bypasses the email check if no email is required.
 function skipEmailValidation() {
     let checker = $('.bypass-email-validation').val();
     if (checker === 'true') {
         emailIsValidFormat = true;
     }
 }
+
+// Event listeners to toggle password input visibility. 
+// New password entry.
+const peekPassword = document.querySelector('.peek-password-input');
+const password = document.querySelector('.password-input');
+if (peekPassword != null) {
+    peekPassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+    });
+}
+// Repeat new password entry.
+const peekPasswordValidation = document.querySelector('.peek-password-validator');
+const passwordValidation = document.querySelector('.password-validator');
+if (peekPasswordValidation != null) {
+    peekPasswordValidation.addEventListener('click', function () {
+        const type = passwordValidation.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordValidation.setAttribute('type', type);
+    });
+}
+// Current password entry.
+const peekPasswordCurrent = document.querySelector('.peek-password-current');
+const passwordCurrent = document.querySelector('.password-current');
+if (peekPasswordCurrent != null) {
+    peekPasswordCurrent.addEventListener('click', function () {
+        const type = passwordCurrent.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordCurrent.setAttribute('type', type);
+    });
+}
+
+
