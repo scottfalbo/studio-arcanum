c9f7fa06a781b17dfd70f03e01c3e6b55a25c471
Scott Falbo <scottfalboart@gmail.com>
2022-02-17 23:47:32 -0800

    scaffolded basic markup for grid layouts, added address model and FK to studio

---
+++ Arcanum/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arcanum.Models
+{
+    public class Address
+    {
+        [ForeignKey("StudioInfo")]
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public StudioInfo StudioInfo { get; set; }
+    }
+}
